2f765a050503429aa1fc22364712d4d7e04080cb
serendipity <1943247190@qq.com>
2023-01-04 13:32:24 +0800

    编译测试

---
+++ fileSystem/function/function.c
@@ -0,0 +1,7 @@
+#include "function.h"
+void initSys(){
+    printf("initSys 执行了\n");
+}
+void sysStart(){
+    printf("sysStart 执行了\n");
+}
---
+++ fileSystem/function/function.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_H
+#define _FUNCTION_H
+void initSys();
+void sysStart();
+#include <stdio.h>
+#endif
